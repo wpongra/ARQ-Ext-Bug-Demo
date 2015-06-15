45686bbd7f6807c4af0b106a39bef68865839577
wpongra <walter.pongratz@ucs.at>
2015-06-15 14:22:42 +0200

    Create DemoArquillian2IT.java

---
+++ src/test/java/com/foo/bar/DemoArquillian2IT.java
@@ -0,0 +1,12 @@
+package com.foo.bar;
+
+import com.foo.bar.deployment.Deployments;
+import org.junit.Test;
+public class DemoArquillian2IT extends Deployments {
+
+
+    @Test
+    public void testSaysHello2() {
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX HELLO 2 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+    }
+}
